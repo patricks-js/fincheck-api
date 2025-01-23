64e4dd857fc319e752088ddf6b10d0afca36fb9e
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2025-01-22 23:59:32 -0300

    make prisma service and save user with a hashed password

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { PrismaService } from "@/database/prisma.service";
+import { Module } from "@nestjs/common";
+import { UsersController } from "./users.controller";
+import { UsersService } from "./users.service";
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, PrismaService],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from "@nestjs/common";
+import { PrismaService } from "@/database/prisma.service";
+import { ConflictException, Injectable } from "@nestjs/common";
+import { hash } from "bcryptjs";
 import { CreateUserDTO } from "./dto/create-user.dto";
 
 @Injectable()
 export class UsersService {
-  async create(data: CreateUserDTO) {
-    return data;
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create({ name, email, password }: CreateUserDTO) {
+    const emailTaken = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (emailTaken) {
+      throw new ConflictException("Email already in use");
+    }
+
+    const passwordHash = await hash(password, 10);
+
+    return this.prisma.user.create({
+      data: { name, email, passwordHash },
+    });
   }
 }
