55570e1f4b170dd8ee6aa7397289a97fad0570f4
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2025-01-23 00:21:55 -0300

    put default categories in the user model when it is created

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -12,14 +12,32 @@ export class UsersService {
       where: { email },
     });
 
-    if (emailTaken) {
-      throw new ConflictException("Email already in use");
-    }
+    if (emailTaken) throw new ConflictException("Email already in use");
 
     const passwordHash = await hash(password, 10);
 
     return this.prisma.user.create({
-      data: { name, email, passwordHash },
+      data: {
+        name,
+        email,
+        passwordHash,
+        categories: {
+          createMany: {
+            data: [
+              // Income
+              { label: "Salário", type: "INCOME", icon: "dollar-sign" },
+              // Expense
+              { label: "Casa", type: "EXPENSE", icon: "home" },
+              { label: "Alimentação", type: "EXPENSE", icon: "croissant" },
+              { label: "Educação", type: "EXPENSE", icon: "graduation-cap" },
+              { label: "Lazer", type: "EXPENSE", icon: "lightbulb" },
+              { label: "Mercado", type: "EXPENSE", icon: "shopping-cart" },
+              { label: "Transporte", type: "EXPENSE", icon: "bus" },
+              { label: "Outros", type: "EXPENSE", icon: "circle-ellipsis" },
+            ],
+          },
+        },
+      },
     });
   }
 }
